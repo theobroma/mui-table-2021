a260218cd907a97faca02a3174842254067e9efc
Alexandr <theobroma222@gmail.com>
2021-03-15 04:25:13 +0200

    currentFilter active

--- src/@components/FilterLink/FilterLink.tsx
+++ src/@components/FilterLink/FilterLink.tsx
@@ -1,8 +1,13 @@
-import { Button } from '@material-ui/core';
 import * as React from 'react';
-import { useDispatch } from 'react-redux';
+import cx from 'classnames';
+import { Button } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
 import { setFilterAC } from '../../@store/filter/slice';
 import { FilterType } from '../../@types';
+import {
+  activeFiltersSelector,
+  filterSelector,
+} from '../../@store/filter/selectors';
 
 interface IProps {
   filterType: FilterType;
@@ -10,9 +15,21 @@ interface IProps {
 
 export const FilterLink: React.FC<IProps> = ({ children, filterType }) => {
   const dispatch = useDispatch();
+  const filterData = useSelector(filterSelector).data;
+  const activeFiltersCount = useSelector(activeFiltersSelector).length;
+
+  const currentFilter = filterData.filter((filter) => {
+    return filter.name === filterType;
+  })[0];
+
   return (
     <Button
       //   type="button"
+      //   className={cx({
+      //     // The user cannot turn off the only included discount type.
+      //     // isDisabled: activeFiltersCount < 2 && currentFilter.active,
+      //   })}
+      color={currentFilter.active ? 'secondary' : undefined}
       onClick={() => dispatch(setFilterAC(filterType))}
       variant="outlined"
     >
---
+++ src/@store/filter/selectors.ts
@@ -0,0 +1,11 @@
+import { RootState } from '..';
+
+export const filterSelector = (state: RootState) => {
+  return state.filter;
+};
+
+export const activeFiltersSelector = (state: RootState) => {
+  return state.filter.data.filter((filter) => {
+    return filter.active;
+  });
+};
