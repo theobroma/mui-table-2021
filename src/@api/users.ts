578bd0865041cac12406d27138ab9bb088e5e878
Alexandr Siryj <theobroma222@gmail.com>
2021-09-20 15:26:56 +0300

    typescript 4.4

--- src/@api/users.ts
+++ src/@api/users.ts
@@ -5,8 +5,12 @@ export const UsersAPI = {
     try {
       const response = await instance.get<any>(`/users`);
       return response.data;
-    } catch (e: any) {
-      return e.message;
+    } catch (e) {
+      // https://stackoverflow.com/a/62611888/3988363
+      if (e instanceof Error) {
+        return e.message;
+      }
+      return false;
     }
   },
 };
